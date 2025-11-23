7fa2f51379614e86f2366fd4c3abcefd21ca3752
Mariana de Paula <maridepaula07@gmail.com>
2025-11-23 12:04:00 -0300

    Update MeuPerfil.aspx.designer.cs

--- MeuPerfil.aspx.designer.cs
+++ MeuPerfil.aspx.designer.cs
@@ -1,12 +1,3 @@
-//------------------------------------------------------------------------------
-// <gerado automaticamente>
-//     Esse código foi gerado por uma ferramenta.
-//
-//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
-//     o código for recriado
-// </gerado automaticamente>
-//------------------------------------------------------------------------------
-
 namespace FastPizza
 {
 
@@ -14,247 +5,112 @@ namespace FastPizza
     public partial class MeuPerfil
     {
 
-        /// <summary>
         /// Controle lblMensagem.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Label lblMensagem;
 
-        /// <summary>
         /// Controle lblErro.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Label lblErro;
 
-        /// <summary>
         /// Controle txtNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtNome;
 
-        /// <summary>
         /// Controle rfvNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvNome;
 
-        /// <summary>
         /// Controle txtEmail.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtEmail;
 
-        /// <summary>
         /// Controle rfvEmail.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvEmail;
 
-        /// <summary>
         /// Controle txtTelefone.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtTelefone;
 
-        /// <summary>
         /// Controle rfvTelefone.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTelefone;
 
-        /// <summary>
         /// Controle btnSalvar.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Button btnSalvar;
 
-        /// <summary>
         /// Controle btnCancelar.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+ 
         protected global::System.Web.UI.WebControls.Button btnCancelar;
 
-        /// <summary>
         /// Controle txtSenhaAtual.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtSenhaAtual;
 
-        /// <summary>
         /// Controle txtNovaSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtNovaSenha;
 
-        /// <summary>
         /// Controle txtConfirmarNovaSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+ 
         protected global::System.Web.UI.WebControls.TextBox txtConfirmarNovaSenha;
 
-        /// <summary>
         /// Controle btnAlterarSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Button btnAlterarSenha;
 
-        /// <summary>
         /// Controle rptEnderecos.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Repeater rptEnderecos;
 
-        /// <summary>
         /// Controle pnlSemEnderecos.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Panel pnlSemEnderecos;
 
-        /// <summary>
         /// Controle lblTituloModal.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+ 
         protected global::System.Web.UI.WebControls.Label lblTituloModal;
 
-        /// <summary>
         /// Controle hdnEnderecoId.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.HiddenField hdnEnderecoId;
 
-        /// <summary>
         /// Controle txtRua.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtRua;
 
-        /// <summary>
         /// Controle txtNumero.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtNumero;
 
-        /// <summary>
         /// Controle txtComplemento.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtComplemento;
 
-        /// <summary>
         /// Controle txtBairro.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtBairro;
 
-        /// <summary>
         /// Controle txtCidade.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtCidade;
 
-        /// <summary>
         /// Controle txtCEP.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtCEP;
 
-        /// <summary>
         /// Controle txtReferencia.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtReferencia;
 
-        /// <summary>
         /// Controle chkEnderecoPadrao.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.CheckBox chkEnderecoPadrao;
 
-        /// <summary>
         /// Controle btnSalvarEndereco.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Button btnSalvarEndereco;
     }
 }
