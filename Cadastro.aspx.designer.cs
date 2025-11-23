c7f0c4ec0f5ef3daafe66b3ca34a387236d28462
Mariana de Paula <maridepaula07@gmail.com>
2025-11-23 12:10:28 -0300

    Update Cadastro.aspx.designer.cs

--- Cadastro.aspx.designer.cs
+++ Cadastro.aspx.designer.cs
@@ -1,12 +1,3 @@
-//------------------------------------------------------------------------------
-// <gerado automaticamente>
-//     Esse código foi gerado por uma ferramenta.
-//
-//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
-//     o código for recriado
-// </gerado automaticamente>
-//------------------------------------------------------------------------------
-
 namespace FastPizza
 {
 
@@ -14,130 +5,60 @@ namespace FastPizza
     public partial class Cadastro
     {
 
-        /// <summary>
         /// Controle lblMensagem.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Label lblMensagem;
 
-        /// <summary>
         /// Controle lblSucesso.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Label lblSucesso;
 
-        /// <summary>
         /// Controle txtNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtNome;
 
-        /// <summary>
         /// Controle rfvNome.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvNome;
 
-        /// <summary>
         /// Controle txtEmail.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtEmail;
 
-        /// <summary>
         /// Controle rfvEmail.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvEmail;
 
-        /// <summary>
         /// Controle revEmail.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RegularExpressionValidator revEmail;
 
-        /// <summary>
         /// Controle txtTelefone.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+  
         protected global::System.Web.UI.WebControls.TextBox txtTelefone;
 
-        /// <summary>
         /// Controle rfvTelefone.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTelefone;
 
-        /// <summary>
         /// Controle txtSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+ 
         protected global::System.Web.UI.WebControls.TextBox txtSenha;
 
-        /// <summary>
         /// Controle rfvSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvSenha;
 
-        /// <summary>
         /// Controle txtConfirmarSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.TextBox txtConfirmarSenha;
 
-        /// <summary>
         /// Controle rfvConfirmarSenha.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvConfirmarSenha;
 
-        /// <summary>
         /// Controle btnCadastrar.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
+  
         protected global::System.Web.UI.WebControls.Button btnCadastrar;
     }
 }
